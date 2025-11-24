a79c7596a1a8216456c9e2f1a5ae556c43de7202
ktsn-ud <153289885+ktsn-ud@users.noreply.github.com>
2025-11-24 22:42:52 +0900

    Remove loop macros from main.cpp and add C++ snippets for easier iteration in VSCode

--- .config/templates/main.cpp
+++ .config/templates/main.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 using ll = long long;
 #define el '\n'
-#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
-#define rrep(i, n) for (int i = (int)(n) - 1; i >= 0; --i)
 
 template <typename T>
 inline void print_vec(const vector<T> &v, bool split_line = false) {
